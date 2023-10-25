0569849dfddb3d0e16123042cc5e9781d5c43885
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2023-10-25 22:44:37 +0600

    Added CatalogContext with Interface and configuration to appsettings

---
+++ src/Services/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,17 @@
+﻿using Catalog.API.Models;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+        }        
+    }
+}
---
+++ src/Services/Catalog.API/Data/ICatalogContext.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.API.Models;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public interface ICatalogContext
+    {
+        public IMongoCollection<Product> Products{ get; }
+    }
+}
