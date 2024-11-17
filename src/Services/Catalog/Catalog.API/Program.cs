3073e82c514ecac3aeadd1af2e1ba7692d48c721
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2024-11-17 23:31:55 +0600

    Setup Docker Orchestration Support And Tested Catalog API With Mongo BD

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
