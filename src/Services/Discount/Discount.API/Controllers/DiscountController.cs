af8346f020ced65f94d33b3815f82f0396613326
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2024-12-06 23:39:22 +0600

    Added Initially Discount API

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,45 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+    [Route("api/v1/[controller]"), ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _repository;
+
+        public DiscountController(IDiscountRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("GetDiscount/{productName}")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            var coupon = await _repository.GetDiscount(productName);
+            return Ok(coupon);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            var response = await _repository.CreateDiscount(coupon);
+            return Ok(coupon);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            await _repository.UpdateDiscount(coupon);
+            return Ok(coupon);
+        }
+
+        [HttpDelete("DeleteDiscount/{productName}")]
+        public async Task<ActionResult<Coupon>> DeleteDiscount(string productName)
+        {
+            var response = await _repository.DeleteDiscount(productName);
+            return Ok(response);
+        }
+    }
+}
