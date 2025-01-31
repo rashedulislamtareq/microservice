958ac78020369b36f730ad9cc2716850bd0b8e94
rashed.tareq@gmail.com <rashed.tareq@gmail.com>
2025-01-31 21:29:55 +0600

    Added Context With Repositories

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain;
+
+namespace Ordering.Infrastructure.Persistence;
+
+public class OrderContextSeed
+{
+    public static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+    {
+        if (!context.Orders.Any()) 
+        {
+            context.Orders.AddRange(GetPreconfiguredOrders());
+            await context.SaveChangesAsync();
+        }
+    }
+
+    private static IEnumerable<Order> GetPreconfiguredOrders()
+    {
+        return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+            };
+    }
+}
